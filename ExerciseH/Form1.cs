d745501480e474fc35d540e1c4a0f07588f8a456
[maruedam] <[miuelangelruedamuriel@gmail.com]>
2018-12-14 16:22:13 +0100

    Ejercicios realizados hasta el i

--- ExerciseH/Form1.cs
+++ ExerciseH/Form1.cs
@@ -23,7 +23,78 @@ namespace ExerciseH
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            textBox1.MaxLength = 1;
+            textBox2.MaxLength = 9;
+
+            comboBox1.Items.Add("camion");
+            comboBox1.Items.Add("moto");
+            comboBox1.Items.Add("coche");
 
         }
+        public class Conductor
+        {
+            public int camion { get; set; }
+            public String moto { get; set; }
+            public enum coche
+            {
+                
+            }
+
+            public Conductor(int camion, string moto)
+            {
+                this.camion = camion;
+                this.moto = moto;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Los cuadros de texto cumplen con sus funciones", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Conductor obj = new Conductor(Convert.ToInt32(textBox1.Text),textBox2.Text);
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else if (Char.IsControl(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else if (Char.IsSeparator(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                MessageBox.Show("Solo se permiten numeros", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+            }
+
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsLetter(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+            {
+                MessageBox.Show("Solo se permiten letras", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+
+        }
+        
+       
     }
+
+       
+           
+    
 }
--- ExerciseI/Form1.cs
+++ ExerciseI/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/*Introducir un botón, cada vez que el ratón se acerque al botón. Este debe desplazarse por la pantalla alejándose del puntero del ratón.*/
 namespace ExerciseI
 {
     public partial class Form1 : Form
@@ -15,6 +15,25 @@ namespace ExerciseI
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Random r = new Random();
+            int x = (int)(this.ClientRectangle.Right * r.NextDouble()) - 30;
+            int y = (int)(this.ClientRectangle.Bottom * r.NextDouble()) - 10;
+            button1.Location = new Point(x, y);
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("LO HAS LOGRADO!!!","!ENHORABUENA¡",MessageBoxButtons.OK,MessageBoxIcon.Warning);
         }
     }
 }
