72e7dd05ab06bce1ab912801811f64245110cdfa
[maruedam] <[miuelangelruedamuriel@gmail.com]>
2018-12-18 08:54:21 +0100

    hasta el ejercicio k

--- ExerciseJ/Form1.cs
+++ ExerciseJ/Form1.cs
@@ -7,7 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/* Crear una clase persona, con nombre, apellido, edad y dni.
+ * Crear un formulario con cuatro cuadros de texto en los cuales introducir distintas personas.
+ * Verificar que no se repiten los dni al introducir el mismo.*/
 namespace ExerciseJ
 {
     public partial class Form1 : Form
@@ -15,6 +17,51 @@ namespace ExerciseJ
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public class Persona
+        {
+            private uint v1;
+            private uint v2;
+
+            public String  nombre{ get; set; }
+            public String  apellido { get; set; }
+            public int edad { get; set; }
+            public int dni { get; set; }
+
+            public Persona(string text, string text1, string text2, object text3)
+            {
+            }
+
+            public Persona(string nombre, string apellido, int edad, int dni)
+            {
+                this.nombre = nombre;
+                this.apellido = apellido;
+                this.edad = edad;
+                this.dni = dni;
+            }
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+                 Persona p1 = new Persona(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text);
+            if (Convert.ToInt32(textBox4.Text)==p1.dni)
+            {
+                MessageBox.Show("El DNI esta reprtido","ERROR",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            else
+            {
+                Persona p2 = new Persona(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text);
+            }
+               
         }
     }
 }
--- ExerciseK/Form1.cs
+++ ExerciseK/Form1.cs
@@ -7,7 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/*Crear una clase familia (Nombre, Id) y una clase articulo(Nombre, IdFamilia, Id).
+ *Crear dos cuadros de texto para la familia y 
+ * tres cuadros de texto para el artículo para insertar información. 
+ * Verificar que el Id no se repita y que el Idfamilia exista para el artículo.*/
 namespace ExerciseK
 {
     public partial class Form1 : Form
@@ -16,5 +19,17 @@ namespace ExerciseK
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        
+
+
+
+
     }
 }
